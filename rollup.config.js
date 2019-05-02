c8487a3466558148204890ba4baf603b31353ab5
Godsenal <tmqps78@gmail.com>
2019-05-02 14:09:59 +0900

    Update to rollup with terser(alternative to uglify)

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+
+  plugins: [typescript(), postcss({ minimize: true }), terser()],
+};
